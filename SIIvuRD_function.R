15800768ebf4504c8fe664b28d25380ff32f90dc
sylvietille <93654204+sylvietille@users.noreply.github.com>
2023-06-14 12:55:17 +0200

    Update SIIvuRD_function.R

--- SIIvuRD_function.R
+++ SIIvuRD_function.R
@@ -10,10 +10,18 @@ time<-function(start=0, end=50, by=0.01){
 }
 
 #parameter values for the model 
-parameters=c(alphaI=0.3,betaI=0.3, vmax=0.1,smax=0., tsi=0.5, d=0.1, dv=0.01,r=0.1,rv=0.1,du=0.1,ru=0.1,dvu=0.01,rvu=0.1,alphaIv=0.5,betaIv=0.5,u=0.0,alphaIu=0.1,betaIu=0.1, alphaIvu=0.3, betaIvu=0.3,delta=-2)
+#alphaI-betaI: effect of vaccine on suscpetible and infected individual
+#alphaIv-betaIv: effect of vaccine on vaccinated suscpetible and vaccinated infected individuals
+#alphaIu-betaIu: effect of vaccine on mutated infected individuals
+#alphaIvu-betaIvu: effect of vaccine on vaccinated mutated infected and vaccinted suscpetible individuals
+#vmax : vaccination rate [0,1], proportion of susceptible vaccinated per unit of time
+#smax: proportion of suxceptible who will never be vaccinated [0,1]
+#d, dv, du, dvu : death rate [0,1]
+#r, rv, ru ,rvu : recovery rate [0,1]
+#delta: virulence of the mutant 
+parameters=c(alphaI=0.,betaI=0., vmax=0.12,smax=0., tsi=1.5, d=0.1, dv=0.01,r=0.1,rv=0.1,du=0.1,ru=0.1,dvu=0.01,rvu=0.1,alphaIv=1,betaIv=1,u=0.0,alphaIu=-0.2,betaIu=-0.2 ,alphaIvu=0.8, betaIvu=0.8,delta=-2)
 
-
-#define original state of groups of the model
+#define initial state of model groups
 #S: Susceptible, Sv: Vaccinated Susceptible, I: Infected, Iv: Vaccinated Infected 
 #D: Death, R: Recovered, Iu: Mutated Infected, Ivu : Vaccinated Mutated Infected
 state=c(S=0.999, Sv=0, I=0.001, Iv=0.0,D=0, R=0, Iu=0.00, Ivu=0.0)
@@ -63,20 +71,7 @@ sirvd_mu <- function(time, state, parameters) {
     legend("center", inset=c(-0.6,-0.30),legend = c("Susceptible ", "Vaccinated Susceptible","Infected","Vaccinated Infected","Death","Recovered",'Mutated infected', 'Mutated Vaccinated infected'), lwd=c(2, 2, 2), col=c("red", "green","orange", "purple","black","blue",'yellow','pink'))
 
   }
-  
-  
-  # plot only the Infected groups of a model (Infected, Vaccinated Infected, Mutated Infected and Mutated Vaccinated Infected )
-  plot_infected_groups<-function(out){
-    par(mfrow = c(1,2))
-    
-    plot(x = out[,1], y = out[,4], type = "l", col="orange", lwd=2, xlab="time", ylab="population size", main="Population", ylim=c(0,1))
-    lines(x = out[,1], y = out[,5], type = "l", col="purple", lwd=2)
-    
-    lines(x = out[,1], y = out[,8], type = "l", col="yellow", lwd=2)
-    lines(x = out[,1], y = out[,9], type = "l", col="pink", lwd=2)
-    plot.new()
-    legend("center", inset=c(-0.6,-0.3),legend = c("Infected","Infected vaccinated",'mutated infected', 'mutated vaccinated infected'), lwd=c(2, 2, 2), col=c("orange", "purple",'yellow','pink'))
-     }
+
   
   #calculate values of the model in time
   cal_model<-function(times, parameters,state){
@@ -90,7 +85,6 @@ sirvd_mu <- function(time, state, parameters) {
   rescu_proba_smax=function(times, state, para,sequ=0.01,N=1000,u=0.01){
     multiplier = seq(0,1,by = sequ) 
     para[4]=1
-    
     
     prob.inv = rep(0,length(multiplier))
     for (i in 1:length(multiplier)){
@@ -140,8 +134,7 @@ sirvd_mu <- function(time, state, parameters) {
 rescu_proba_vmax=function(times, state, para,sequ=0.01,N=1000,u=0.01){
   multiplier = seq(0,1,by = sequ) 
   para[3]=1
-  
-  
+
   prob.inv = rep(0,length(multiplier))
   for (i in 1:length(multiplier)){
     
@@ -151,8 +144,7 @@ rescu_proba_vmax=function(times, state, para,sequ=0.01,N=1000,u=0.01){
     dt = 0.01
     names(out) = c("Time","Susceptible","Vaccinated Susceptible", "Infected", "Vaccinated Infected", "Death", "Recovered","Mutated infected", "Mutated Vaccinated infected")
     dat.SIR = melt(out,id="Time",measure = c("Susceptible","Vaccinated Susceptible", "Infected", "Vaccinated Infected", "Death", "Recovered","Mutated infected", "Mutated Vaccinated infected"))
-    
-    
+
     names(dat.SIR) = c("Time","Compartment","Value")
     
     S = filter(dat.SIR,Compartment == "Susceptible")
@@ -179,13 +171,10 @@ rescu_proba_vmax=function(times, state, para,sequ=0.01,N=1000,u=0.01){
     
     prob.inv[i] = 1-prod(1-(I$Value*Iu_growthrate+Iv$Value*Ivu_growthrate)*N*u*dt)
   }  
-  
-  
+
   plot(para[3]*multiplier,prob.inv,type="l",xlab = "vmax",main = "",xlim=c(0,0.2))
-  
   return(prob.inv)
 }
-
 
 #calculate the number of death compare to vmax and plot it 
 death_end_vmax=function(times, para,state, sequ=0.01){
@@ -210,12 +199,6 @@ death_end_vmax=function(times, para,state, sequ=0.01){
 
 model=cal_model(times, parameters, state)
 plot_all_groups(model)
-
-
-plot_infected_groups(model)
 v=rescu_proba_vmax(times,state,parameters,sequ=0.01,u=10^-6,N=100000)
 d=death_end_vmax(times,parameters,state,0.01)
 s=rescu_proba_smax(times,state,parameters,sequ=0.01,u=1/1000000,N=100000)
-
-
-
